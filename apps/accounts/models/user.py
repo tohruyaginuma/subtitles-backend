73ae675d4b29eaa48e1b5314acba893304cb0177
Tohru Yaginuma <toru45s@gmail.com>
2025-08-19 19:43:01 -0700

    update models and migrations

--- apps/accounts/models/user.py
+++ apps/accounts/models/user.py
@@ -6,6 +6,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
